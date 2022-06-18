d8cb54cbefb3ae9b10df20a9014969b37a3f9747
abenito <abenito.org@gmail.com>
2022-06-18 16:08:01 +0100

    Build

--- assets/js/service-worker.js
+++ assets/js/service-worker.js
@@ -29,7 +29,7 @@ const SHELL_CACHE = "shell-9.0.4--v7--sw/AlphaBetEcon/";
 const ASSETS_CACHE = "assets--v7--sw/AlphaBetEcon/";
 
 // The cache for regular content, which will be invalidated every time you make a new build.
-const CONTENT_CACHE = "content--2022-06-18T16:04:57+01:00--sw/AlphaBetEcon/";
+const CONTENT_CACHE = "content--2022-06-18T16:07:26+01:00--sw/AlphaBetEcon/";
 
 // A URL search parameter you can add to external assets to cache them in the service worker.
 const SW_CACHE_SEARCH_PARAM = "sw-cache";
