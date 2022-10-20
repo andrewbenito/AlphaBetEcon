991c30efe9572e15824fa0055003c39b82a305ae
abenito <abenito.org@gmail.com>
2022-10-20 10:59:50 +0100

    Build

--- assets/js/search-worker-9.0.4.js
+++ assets/js/search-worker-9.0.4.js
@@ -47,7 +47,7 @@ async function getDocuments() {
 ///////////////////////////////////////////////////////////////////////////////
 
 importScripts('/AlphaBetEcon/assets/js/minisearch/dist/umd/index.min.js');
-const INDEX_KEY = 'index--2022-10-20T10:48:13+01:00';
+const INDEX_KEY = 'index--2022-10-20T10:59:26+01:00';
 const storage = new StorageArea('mini-search/AlphaBetEcon/');
 
 const OPTIONS = {
