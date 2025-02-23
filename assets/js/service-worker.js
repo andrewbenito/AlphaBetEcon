91fe639571beb9cba4a3361c9a0b68224330534b
abenito <abenito.org@gmail.com>
2025-02-23 11:08:00 +0000

    Add publication

--- assets/js/service-worker.js
+++ assets/js/service-worker.js
@@ -29,7 +29,7 @@ const SHELL_CACHE = "shell-9.0.4--v7--sw/AlphaBetEcon/";
 const ASSETS_CACHE = "assets--v7--sw/AlphaBetEcon/";
 
 // The cache for regular content, which will be invalidated every time you make a new build.
-const CONTENT_CACHE = "content--2025-02-08T15:34:27+00:00--sw/AlphaBetEcon/";
+const CONTENT_CACHE = "content--2025-02-23T11:06:43+00:00--sw/AlphaBetEcon/";
 
 // A URL search parameter you can add to external assets to cache them in the service worker.
 const SW_CACHE_SEARCH_PARAM = "sw-cache";
@@ -53,11 +53,12 @@ const SHELL_FILES = [
 ];
 
 const STATIC_FILES = [
-  /**/"/AlphaBetEcon/assets/00---UK-macro-presentation.pdf",
   /**/"/AlphaBetEcon/assets/BenitoYoungTalk.pdf",
   /**/"/AlphaBetEcon/assets/BoEDoesIT.pdf",
   /**/"/AlphaBetEcon/assets/Brexit.pdf",
+  /**/"/AlphaBetEcon/assets/CVabRmd.log",
   /**/"/AlphaBetEcon/assets/CVabRmd.pdf",
+  /**/"/AlphaBetEcon/assets/NIER-Benito-Young.pdf",
   /**/"/AlphaBetEcon/assets/Working_in_Finance.pdf",
   /**/"/AlphaBetEcon/assets/benitoYoungIZA.pdf",
   /**/"/AlphaBetEcon/assets/benitoYoungUpdate.pdf",
