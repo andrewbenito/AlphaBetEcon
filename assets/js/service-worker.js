d8d85578e1e92f0a05a581227b0bf11372555eb9
abenito <abenito.org@gmail.com>
2025-03-01 13:53:54 +0000

    Build it

--- assets/js/service-worker.js
+++ assets/js/service-worker.js
@@ -29,7 +29,7 @@ const SHELL_CACHE = "shell-9.0.4--v7--sw/AlphaBetEcon/";
 const ASSETS_CACHE = "assets--v7--sw/AlphaBetEcon/";
 
 // The cache for regular content, which will be invalidated every time you make a new build.
-const CONTENT_CACHE = "content--2025-03-01T13:29:34+00:00--sw/AlphaBetEcon/";
+const CONTENT_CACHE = "content--2025-03-01T13:53:01+00:00--sw/AlphaBetEcon/";
 
 // A URL search parameter you can add to external assets to cache them in the service worker.
 const SW_CACHE_SEARCH_PARAM = "sw-cache";
@@ -53,6 +53,7 @@ const SHELL_FILES = [
 ];
 
 const STATIC_FILES = [
+  /**/"/AlphaBetEcon/assets/00---UK-macro-presentation.pdf",
   /**/"/AlphaBetEcon/assets/BenitoYoungTalk.pdf",
   /**/"/AlphaBetEcon/assets/BoEDoesIT.pdf",
   /**/"/AlphaBetEcon/assets/Brexit.pdf",
