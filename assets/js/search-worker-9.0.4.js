c5da589ee53f8e265baaf0e0df7214a74d1c1e47
abenito <abenito.org@gmail.com>
2025-05-11 08:34:30 +0100

    Build watchers

--- assets/js/search-worker-9.0.4.js
+++ assets/js/search-worker-9.0.4.js
@@ -47,7 +47,7 @@ async function getDocuments() {
 ///////////////////////////////////////////////////////////////////////////////
 
 importScripts('/AlphaBetEcon/assets/js/minisearch/dist/umd/index.min.js');
-const INDEX_KEY = 'index--2025-04-26T14:24:08+01:00';
+const INDEX_KEY = 'index--2025-05-11T08:33:52+01:00';
 const storage = new StorageArea('mini-search/AlphaBetEcon/');
 
 const OPTIONS = {
